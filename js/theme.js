62390c8615b178a2ba0960596eb25fb59eef309f
shiming <siminily@gmail.com>
2015-10-24 20:56:41 +0800

    Bootstrap

---
+++ js/theme.js
@@ -0,0 +1,21 @@
+$(function (){
+	//color
+	$(".aqua").css("color","aque");
+	$(".black").css("color","black");
+	$(".blue").css("color","blue");
+	$(".fuchsia").css("color","fuchsia");
+	$(".gray").css("color","gray");
+	$(".green").css("color","green");
+	$(".lime").css("color","lime");
+	$(".maroon").css("color","maroon");
+	$(".navy").css("color","navy");
+	$(".olive").css("color","olive");
+	$(".purple").css("color","purple");
+	$(".red").css("color","red");
+	$(".teal").css("color","silver");
+	$(".white").css("color","white");
+	$(".yellow").css("color","yellow");
+	//margin
+	$(".m5").css("margin","5");
+	$.scrollUp({scrollText: '<i class="icon-arrow-up"></i>'});
+});
